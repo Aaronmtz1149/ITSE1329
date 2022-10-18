7a253764e6c9170e3a2cc4563f09d1b25ad1bc40
aaronmtz1149 <aaronmtz1149@gmail.com>
2022-10-18 08:34:20 -0500

    First Commit

---
+++ src/main/kotlin/Week02/AdvertisingConditional.kt
@@ -0,0 +1,16 @@
+package week02
+
+fun main()
+{
+    /* You don't need to declare the variable all the time (line 13-14)*/
+    var friendsCount = 300
+    println(friendsCount)
+    if(friendsCount>500)
+    {
+        println("Adopt a dog today!!")
+    }else
+    {
+        println("Buy cat food at 20%")
+    }
+
+}
