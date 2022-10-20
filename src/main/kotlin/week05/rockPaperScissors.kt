55bdf4433a72b2b45a6d86a3a865b4c9a0409d82
aaronmtz1149 <aaronmtz1149@gmail.com>
2022-10-20 09:27:21 -0500

    First Commit

--- src/main/kotlin/week05/rockPaperScissors.kt
+++ src/main/kotlin/week05/rockPaperScissors.kt
@@ -7,34 +7,38 @@ import kotlin.random.Random
  * Assignment 05.7 Rock Paper Scissors Game
  * October 19, 2022
  */
-var answer:Int=0
 var botAnswer:Int=0
 var botChoice=""
 var choice:String=""
-var options= arrayOf("Rock","Paper","Scissors")
+var options= arrayOf("rock","paper","scissors")
 
 fun main(){
     userChooses()
     botChooses()
-    print("Bot chose $botAnswer - $botChoice and you chose $answer - $choice")
 }
 fun userChooses(){
-    print("Enter your choice (1-3):\n(1). Rock\n(2). Paper\n(3). Scissors\n")
-    answer=readln().toInt()
-    choice= options[answer-1]
+    println("Enter your choice:\n(1). Rock\n(2). Paper\n(3). Scissors\n")
+    choice=readln().toLowerCase()
 }
-fun botChooses(){
+fun botChooses() {
     //import Random class
-    botAnswer= Random.nextInt(1,3)
-    botChoice=options[botAnswer-1]
-    while(botAnswer == answer){
+    botAnswer = Random.nextInt(1, 3)
+    botChoice = options[botAnswer - 1]
+    while (botChoice == choice) {
         println("It's a tie!! Try again!")
+        println("You chose $choice and bot chose $botChoice")
         userChooses()
-        botAnswer= Random.nextInt(1,3)
-        botChoice=options[botAnswer-1]
+        botAnswer = Random.nextInt(0, 3)
+        botChoice = options[botAnswer]
     }
-    if((botAnswer==1&&answer==2)||(botAnswer==2&&answer==3)||(botAnswer==3&&answer==1)){
+    if ((botChoice == options[0] && choice == options[1]) || (botChoice == options[1] && choice == options[2]) || (botChoice == options[2] && choice == options[0])) {
         println("You win! $choice beats $botChoice")
-    }else println("You lose! $botChoice beats $choice")
+        print("You chose $choice and bot chose $botChoice")
+    } else
+        if ((botChoice == options[0] && choice == options[2]) || (botChoice == options[1] && choice == options[0]) || (botChoice == options[2] && choice == options[1])) {
+            println("You lose! $botChoice beats $choice")
+            print("Bot chose $botChoice and you chose $choice")
+        } else
+            println("Invalid entry!! Try again")
 
-}
+    }
