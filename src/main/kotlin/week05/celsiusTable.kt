612aeb901da9f2845607d027a8eb5e5258a24fde
aaronmtz1149 <aaronmtz1149@gmail.com>
2022-10-20 08:55:02 -0500

    First Commit

--- src/main/kotlin/week05/celsiusTable.kt
+++ src/main/kotlin/week05/celsiusTable.kt
@@ -9,7 +9,7 @@ fun main(){
     celsius(20)
 }
 fun celsius(f:Int){
-    for(x in 1..f){
+    for(x in 0..f){
         var celsius=(5.0/9)*(x-32)
         println("$x degrees Farenheit is $celsius degrees Celsius")
     }
