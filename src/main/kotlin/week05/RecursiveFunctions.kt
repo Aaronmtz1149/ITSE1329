34b68930bea19c364df3f542fecd1c462177eece
aaronmtz1149 <aaronmtz1149@gmail.com>
2022-10-20 08:38:07 -0500

    First Commit

--- src/main/kotlin/week05/RecursiveFunctions.kt
+++ src/main/kotlin/week05/RecursiveFunctions.kt
@@ -1,13 +1,17 @@
 package week05
 
 /**
+ * Aaron M
  * Assignment 05.2 Recursive Functions
  * October 18, 2022
  */
 fun main(){
     print("Enter an integer >> ")
     var number=readln().toInt()
-println("fibonacci($number)==${fibonacci(number)}")
+    println("fibonacci($number)==${fibonacci(number)}")
+    println()
+    for(x in 1..number)
+        println("fibonacci($x)==${fibonacci(x)}")
 }
 fun fibonacci(number:Int):Int{
     var x=0
