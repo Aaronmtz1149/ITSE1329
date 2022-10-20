b3a2c912cc3a0487c9347117e43973ea1757bf13
aaronmtz1149 <aaronmtz1149@gmail.com>
2022-10-20 08:50:08 -0500

    First Commit

--- src/main/kotlin/week05/priceCalculator.kt
+++ src/main/kotlin/week05/priceCalculator.kt
@@ -5,14 +5,16 @@ package week05
  * Assignment 05.3
  * October 18, 2022
  */
+    var markup=0.0
+    var price=0.0
 fun main(){
     print("Enter the original price of the item >> ")
-    var price = readln().toDouble()
+    price = readln().toDouble()
     print("Enter the markup percentage of the item >> ")
-    var markup=readln().toDouble()
+    markup=readln().toDouble()
     calculateRetail(price,markup)
 }
 fun calculateRetail(number:Double,percentage:Double){
     var total=number*(1+(percentage/100))
-    print(total)
+    println("The markup price of $price is $$total")
 }
