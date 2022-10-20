b8c1908afc9935ec082d5022048e3a135737781c
aaronmtz1149 <aaronmtz1149@gmail.com>
2022-10-20 10:28:54 -0500

    First Commit

--- src/main/kotlin/Week02/CompoundInterest.kt
+++ src/main/kotlin/Week02/CompoundInterest.kt
@@ -7,19 +7,21 @@ fun main()
     var P = readln().toDouble()
     print("Enter the annual interest rate >> ")
     var rate = readln().toDouble()
-    var ratePercent = rate*100
+    var ratePercent = rate/100
     print("How many times per year will the interest compound >> ")
     var n = readln().toDouble()
     print("How many years do you want to accumulate interest for? >> ")
     var years = readln().toDouble()
     var power = n*years
-    var a = P * Math.pow((1 + rate / n),(power))
+    var a = P * ((1 + (ratePercent / n)).pow((n*years)))
     if(n==4.0)
     {
         println("With a principal $$P deposit and a quarterly rate interest compound of $ratePercent%,\nthe accumulated amount at the end of $years years is $$a")
     }else if(n==12.0)
     {
         println("With a principal $$P deposit and a monthly rate interest compound of $ratePercent%,\nthe accumulated amount at the end of $years years is $$a")
+    }else{
+        print("Invalid entry.\nYou must enter 12 for monthly\nor 4 for quarterly")
     }
 
 }
