7a43cfc98bfd13f31b1b7c1c2525c06cb7c000ea
aaronmtz1149 <aaronmtz1149@gmail.com>
2022-11-01 08:43:24 -0500

    First Commit

---
+++ src/main/kotlin/week01/error.kt
@@ -0,0 +1,13 @@
+fun main(){
+    /* CODE WITH ERROR
+    var width = 5
+    var height = 5
+    var area = width + height
+    print(area)*/
+
+    //code fixed to find area
+    val width=5
+    val height=5
+    val area=width*height
+    print(area)
+}
