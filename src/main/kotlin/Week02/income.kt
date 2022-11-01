0a0b1dc9d19a4ac51a08763e0cd5bcb02b684d98
aaronmtz1149 <aaronmtz1149@gmail.com>
2022-11-01 08:37:38 -0500

    First Commit

--- src/main/kotlin/Week02/income.kt
+++ src/main/kotlin/Week02/income.kt
@@ -3,5 +3,8 @@ fun main()
     var name = "Stephen Curry"
     var age = 34
     var annualPay = 45.78
+    var kobe="kobe"
+    var l='l'
+
     println("My name is $name, my age is $age and I hope to earn $$annualPay million per year")
 }
---
+++ src/main/kotlin/week01/hello.kt
@@ -0,0 +1,5 @@
+fun main(){
+    print("Hello world")
+    println("I've completed this before, but it got lost :(")
+    println("Learning Koltin is awesome")
+}
